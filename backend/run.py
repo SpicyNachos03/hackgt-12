15f4e67af4a9d030efb586fa3f60944f9b936859
laury64368 <131693578+laury64368@users.noreply.github.com>
2025-09-27 08:42:32 -0400

    lauren branch

--- backend/run.py
+++ backend/run.py
@@ -24,6 +24,6 @@ if __name__ == '__main__':
     
     app.run(
         host='0.0.0.0',
-        port=int(os.environ.get('PORT', 5000)),
+        port=int(os.environ.get('PORT', 5001)),
         debug=True if env == 'development' else False
     )
