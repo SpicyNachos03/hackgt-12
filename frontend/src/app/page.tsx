a2aac683b3c3bcaeda037cefc6bf16375eac64b9
SpicyNachos03 <122487143+SpicyNachos03@users.noreply.github.com>
2025-09-27 18:46:33 -0400

    auth works?

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -74,7 +74,7 @@ export default function Home() {
           <p style={{ color: '#ffffff' }}>Card 3 content goes here.</p>
         </SpotlightCard>
       </div>
-
+      <a href="/auth/login">Login</a>
         <Stepper
       initialStep={1}
       onStepChange={(step) => {
