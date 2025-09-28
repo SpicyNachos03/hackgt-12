25d68c618c734dcd7f2742ad46030d8228a7a7fc
laury64368 <131693578+laury64368@users.noreply.github.com>
2025-09-27 22:29:33 -0400

    input

--- frontend/src/app/routes/chat-page/page.tsx
+++ frontend/src/app/routes/chat-page/page.tsx
@@ -1,34 +1,38 @@
 "use client";
 import { useRouter } from "next/navigation";
 import { CedarCaptionChat } from "../../../cedar/components/chatComponents/CedarCaptionChat";
-import { TabView, TabPanel } from "primereact/tabview";
-import "primereact/resources/themes/tailwind-light/theme.css";
-
+import { PrimeReactProvider, PrimeReactContext } from 'primereact/api';
+import React from 'react'; 
+import { InputText } from 'primereact/inputtext';
+import { InputNumber } from 'primereact/inputnumber';
+import "primereact/resources/themes/bootstrap4-dark-purple/theme.css"
 
 export default function ChatPage() {
-  const router = useRouter();
+
+
   return (
     <div>
-    <button onClick={() => window.location.href = '/auth/logout'}>
-      Logout
-    </button>
-      <TabView>
-        <TabPanel header="Header I">
-          <p className="m-0">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua...
-          </p>
-        </TabPanel>
-        <TabPanel header="Header II">
-          <p className="m-0">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium...
-          </p>
-        </TabPanel>
-        <TabPanel header="Header III">
-          <p className="m-0">
-            At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium...
-          </p>
-        </TabPanel>
-      </TabView>
+      <PrimeReactProvider>
+        <div style={{ marginTop: "5%" }}>
+        <div className="card flex flex-column md:flex-row gap-3">
+            <div className="p-inputgroup flex-1">
+                <span className="p-inputgroup-addon">1.</span>
+                <InputNumber placeholder="Paitent ID" />
+            </div>
+
+            <div className="p-inputgroup flex-1">
+                <span className="p-inputgroup-addon">2.</span>
+                <InputText placeholder="Drugs" />
+            </div>
+
+            <div className="p-inputgroup flex-1">
+                <span className="p-inputgroup-addon">3.</span>
+                <InputText placeholder="Symptoms" />
+            </div>
+        </div>
+        </div>
+        
+        </PrimeReactProvider>
     </div>
   );
 }
